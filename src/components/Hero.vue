c12098784769288fc9b5d84a49bf02b7f3f6e28d
Ian Torres <iantorres@outlook.com>
2025-05-22 22:55:58 -0400

    Bump.

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -10,10 +10,13 @@ const isDark = useDark()
       :class="isDark ? 'bg-black text-white' : 'bg-white text-black'"
   >
     <img
-        :src="isDark ? '/logo-white.png' : '/logo-black.png'"
+        src="/logo.png"
         alt="Throttr Logo"
-        class="h-auto mb-8 transition-all duration-500"
+        class="h-80 mb-8 transition-all duration-500"
     />
+    <h2 class="tracking-wide mb-24 bg-gradient-to-br from-orange-700 to-orange-900 bg-clip-text text-7xl md:text-9xl font-extrabold text-transparent">
+      Throttr
+    </h2>
     <p class="mt-4 text-2xl font-light tracking-wide leading-16" :class="isDark ? 'text-gray-400' : 'text-gray-600'">
       Master the memory.
       The limit is set by your machine.
